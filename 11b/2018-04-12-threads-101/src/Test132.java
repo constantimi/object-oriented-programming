491c9328ae8a5533f292ef2ff40942a72f70e06a
Constantin <mihailovkonstantin364@gmail.com>
2018-04-24 11:07:03 +0300

    stream

---
+++ 11b/2018-04-12-threads-101/src/Test132.java
@@ -0,0 +1,22 @@
+
+
+class Foo{
+	Foo(String s){
+		System.out.print(s);
+	}
+}
+
+class Bar extends Foo{
+
+	Bar(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	} }
+
+public class Test132 {
+ public static void main() {
+	 new Foo("Foo");
+	 new Bar(null);
+	 System.out.print(" done");
+ }
+}
